0fbfa140754635ac1818533e51fa7d75b131763c
Michael Colavita <colavitam@gmail.com>
2018-04-16 23:15:24 -0400

    Distributed channel with monitors

--- com/cs262/dobj/test/Main.java
+++ com/cs262/dobj/test/Main.java
@@ -1,13 +1,13 @@
 package com.cs262.dobj.test;
 
 import com.cs262.dobj.*;
-import java.io.Serializable;
+import java.io.*;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     BST<String, String> bst = new RedBlackBST<String, String>();
 
-    DistributedChannel myChannel = new SocketDistributedChannel();
+    DistributedChannel myChannel = new SocketDistributedChannel(9091);
     DistributedContext myContext = new DistributedContext(myChannel);
     BST<String, String> dbst = myContext.createDistributedInstance(bst, bst.getClass().getInterfaces());
     System.out.println(dbst.isEmpty());
