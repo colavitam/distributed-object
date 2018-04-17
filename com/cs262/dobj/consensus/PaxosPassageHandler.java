002a4c48385c1c3bffef64fbd143a126af2fb9a8
Eric Lu <penlume@gmail.com>
2018-04-16 22:03:53 -0400

    paxos parametric in method not class

--- com/cs262/dobj/consensus/PaxosPassageHandler.java
+++ com/cs262/dobj/consensus/PaxosPassageHandler.java
@@ -3,6 +3,6 @@ package com.cs262.dobj.consensus;
 import com.cs262.dobj.Message;
 import java.io.*;
 
-public interface PaxosPassageHandler<T extends Serializable> {
-  public void handlePassage(long decreeNumber, Message<T> decreeValue);
+public interface PaxosPassageHandler {
+  public <T extends Serializable> void handlePassage(long decreeNumber, Message<T> decreeValue);
 }
--- com/cs262/dobj/consensus/PaxosProtocol.java
+++ com/cs262/dobj/consensus/PaxosProtocol.java
@@ -3,7 +3,7 @@ package com.cs262.dobj.consensus;
 import com.cs262.dobj.Message;
 import java.io.*;
 
-public interface PaxosProtocol<T extends Serializable> {
-  public long request(Message<T> value) throws IOException;
-  public void registerPassageHandler(PaxosPassageHandler<T> handler);
+public interface PaxosProtocol {
+  public <T extends Serializable> long request(Message<T> value) throws IOException;
+  public <T extends Serializable> void registerPassageHandler(PaxosPassageHandler<T> handler);
 }
